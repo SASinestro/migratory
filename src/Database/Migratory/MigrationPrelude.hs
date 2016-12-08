1e7da40d7ba9b60b24ed3e06d4e77c6155e1ebb3
Alexis Williams <sasinestro@gmail.com>
2016-12-08 12:32:04 -0800

    Add Prelude replacement for Migration files

---
+++ src/Database/Migratory/MigrationPrelude.hs
@@ -0,0 +1,12 @@
+module Database.Migratory.MigrationPrelude (module Prelude, module Database.Migratory.TableSchema, module Control.Monad.Indexed, (>>>), ifThenElse) where
+
+import Control.Monad.Indexed
+import Database.Migratory.TableSchema
+import Prelude                        hiding (Monad (..))
+
+(>>>) :: forall a b i j k m. IxMonad m => m i j a -> m j k b -> m i k b
+(>>>) = \a b -> a >>>= (\_ -> b)
+
+ifThenElse :: Bool -> a -> a -> a
+ifThenElse True a _  = a
+ifThenElse False b _ = b
