c368b42a52b90885104c896f0cff936ed22082a9
Alexis Williams <sasinestro@gmail.com>
2016-12-08 17:47:36 -0800

    Add shortcuts to MigrationPrelude

--- src/Database/Migratory/MigrationPrelude.hs
+++ src/Database/Migratory/MigrationPrelude.hs
@@ -1,12 +1,19 @@
-module Database.Migratory.MigrationPrelude (module Prelude, module Database.Migratory.TableSchema, module Control.Monad.Indexed, (>>>), ifThenElse) where
+module Database.Migratory.MigrationPrelude (module Prelude, module Database.Migratory.Schema, module Control.Monad.Indexed, ifThenElse, tName, col, cName) where
 
 import Control.Monad.Indexed
 import Database.Migratory.Schema
+import GHC.TypeLits
 import Prelude                        hiding (Monad (..))
-
-(>>>) :: forall a b i j k m. IxMonad m => m i j a -> m j k b -> m i k b
-(>>>) = \a b -> a >>>= (\_ -> b)
 
 ifThenElse :: Bool -> a -> a -> a
 ifThenElse True a _  = a
 ifThenElse False b _ = b
+
+tName :: KnownSymbol tblName => TableName tblName
+tName = TableName
+
+col :: KnownSymbol name => Column name ty cons
+col = Column
+
+cName :: KnownSymbol name => ColumnName name
+cName = ColumnName
