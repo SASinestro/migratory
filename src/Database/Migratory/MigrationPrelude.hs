c2e17026a9c7addf71d84db4cda18e761a9b3da4
Alexis Williams <sasinestro@gmail.com>
2016-12-09 20:19:34 -0800

    Drop use of very limited-need library.

--- src/Database/Migratory/MigrationPrelude.hs
+++ src/Database/Migratory/MigrationPrelude.hs
@@ -1,4 +1,4 @@
-module Database.Migratory.MigrationPrelude (module Prelude, module Database.Migratory.Schema, module Control.Monad.Indexed, ifThenElse, tName, col, cName) where
+module Database.Migratory.MigrationPrelude (module Prelude, module Database.Migratory.Schema, module Control.Monad.Indexed, ifThenElse, return, (>>=), (>>), tName, col, cName) where
 
 import           Control.Monad.Indexed
 import           Database.Migratory.Schema
@@ -8,6 +8,15 @@ import           Prelude                   hiding (Monad (..))
 ifThenElse :: Bool -> a -> a -> a
 ifThenElse True a _  = a
 ifThenElse False b _ = b
+
+return :: forall a i m. (IxMonad m) =>  a -> m i i a
+return = ireturn
+
+(>>=) :: forall a b i j k m. (IxMonad m) => m i j a -> (a -> m j k b) -> m i k b
+(>>=) = (>>>=)
+
+(>>) :: forall a b i j k m. (IxMonad m) => m i j a -> m j k b -> m i k b
+(>>) = \a b -> a >>>= (\_ -> b)
 
 tName :: KnownSymbol tblName => TableName tblName
 tName = TableName
